0ccc09e7840306affbab84b0b2712f00268be299
Chonticha Chang <Ruttikar_maya@hotmail.com>
2017-07-13 01:09:06 +0700

    Add Quartz + update database : no problem here

---
+++ VA/Service/SMSSchedule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using Quartz.Impl;
+using System.IO;
+using System.Web.UI;
+
+namespace VA.Service
+{
+    public class SMSSchedule
+    {
+    /*    public static void Start()
+        {
+            IJobDetail SMSJob = JobBuilder.Create<SMSService>()
+                .WithIdentity("SMS")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                (s =>
+                s.WithIntervalInMinutes(1)
+                .OnEveryDay()
+            )
+            .ForJob(SMSJob)
+            .WithIdentity("trigger")
+            .StartNow()
+            .WithCronSchedule("0 0/1 * * * ?")
+            .Build();
+
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            IScheduler sc = sf.GetScheduler();
+            sc.ScheduleJob(SMSJob, trigger);
+            sc.Start();
+        }*/
+    }
+}
